53824214beb9cb45bff48dbd0ab1611e0e48d50a
Amber Singh <ambersinghrok@gmail.com>
2025-02-03 10:50:23 -0500

    Time: 0 ms (100.00%) | Memory: 27.9 MB (64.71%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     int longestMonotonicSubarray(vector<int>& a) {
-        int incCnt = 0, decCnt = 0, maxi = INT_MIN, mini = INT_MIN;
         int n = a.size();
-        for (int i = 0; i < n; i++) {
-            if (i == 0 || (i > 0 && a[i] == a[i - 1])) {
-                incCnt = 1;
-                decCnt = 1;
-                maxi = max(maxi, incCnt);
-                mini = max(mini, decCnt);
-            }
-            if (i > 0 && a[i] > a[i - 1]) {
+        if (n == 0) return 0;
+        
+        int incCnt = 1, decCnt = 1, maxLen = 1;
+        
+        for (int i = 1; i < n; i++) {
+            if (a[i] > a[i - 1]) {
                 incCnt++;
-                decCnt = 1;
-                maxi = max(maxi, incCnt);
-            }
-            if (i > 0 && a[i] < a[i - 1]) {
+                decCnt = 1;  
+            } 
+            else if (a[i] < a[i - 1]) {
                 decCnt++;
-                incCnt = 1;
-                mini = max(mini, decCnt);
+                incCnt = 1; 
+            } 
+            else {
+                incCnt = decCnt = 1;
             }
+
+            maxLen = max(maxLen, max(incCnt, decCnt));
         }
-        return max(maxi, mini);
+
+        return maxLen;
     }
 };
