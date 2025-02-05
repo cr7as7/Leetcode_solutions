1fd37dbc00cf9cd1b17d4483fea1acca0cb12835
Amber Singh <ambersinghrok@gmail.com>
2025-02-05 08:52:47 -0500

    Time: 0 ms (100.00%) | Memory: 8.2 MB (97.48%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
-        int n = s1.size();
-        if (s1 == s2)
-            return true;
-        int cnt = 0, idx1 = -1, idx2 = -1;
-        for (int i = 0; i < n; i++) {
-            if (s1[i] != s2[i] && idx1 == -1) {
-                idx1 = i;
-            }
-            if (s1[i] != s2[i] && idx1 != -1) {
-                idx2 = i;
+        if (s1 == s2) return true; // Already equal
+        
+        int idx1 = -1, idx2 = -1, diffCount = 0;
+        
+        for (int i = 0; i < s1.size(); i++) {
+            if (s1[i] != s2[i]) {
+                diffCount++;
+                if (idx1 == -1) idx1 = i;
+                else if (idx2 == -1) idx2 = i;
+                else return false; // More than 2 differences → cannot be fixed with 1 swap
             }
         }
-        if (idx1 != -1 && idx2 != -1) {
-            swap(s2[idx1], s2[idx2]);
-            return (s1 == s2);
-        }
-        return false;
+        
+        return (diffCount == 2 && s1[idx1] == s2[idx2] && s1[idx2] == s2[idx1]);
     }
 };
