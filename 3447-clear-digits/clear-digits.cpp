54d743381b149a3c1bc4c843e0bbc217a8b3e0a0
Amber Singh <ambersinghrok@gmail.com>
2025-02-11 07:22:54 -0500

    Time: 0 ms (100.00%) | Memory: 8.4 MB (87.07%) - LeetSync

--- 3447-clear-digits/clear-digits.cpp
+++ 3447-clear-digits/clear-digits.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
+    void inplace(string & s){
+        int ans_len=0;
+        int n = s.size();
+        for(int i=0;i<n;i++){
+            if(isdigit(s[i])){
+                ans_len = max(ans_len - 1,0);
+            }
+            else{
+                s[ans_len] = s[i];
+                ans_len++;
+            }
+        }
+        s.resize(ans_len);
+
+    }
     string clearDigits(string s) {
+        inplace(s);
+        return s;
+
         stack<char> st;
         string ans = "";
         for (auto i : s) {
@@ -16,6 +34,7 @@ public:
             st.pop();
         }
         reverse(ans.begin(),ans.end());
+
         return ans;
     }
 };
