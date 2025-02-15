665c492112d7b81b66389b32b41289a82f1bcc62
Amber Singh <ambersinghrok@gmail.com>
2025-02-15 09:41:44 -0500

    Time: 1551 ms (5.04%) | Memory: 418.1 MB (10.83%) - LeetSync

--- 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
@@ -1,23 +1,27 @@
 class Solution {
-public:
+public: // sq,idx,sum
+    unordered_map<int, unordered_map<int, bool>> mp;
     bool check(string& sq, int num, int i, int currsum) {
         int n = sq.size();
         if (i == n && currsum == num)
-            return true;
+            return mp[i][currsum] = true;
         else if (i == n)
-            return false;
-
+            return mp[i][currsum] = false;
+        if (mp[i].count(currsum))
+            return mp[i][currsum];
         for (int j = i; j < n; j++) {
             string s = sq.substr(i, j - i + 1);
             if (check(sq, num, j + 1, currsum + stoi(s)))
-                return true;
+                return mp[i][currsum] = true;
         }
-        return false;
+        return mp[i][currsum] = false;
     }
     int punishmentNumber(int n) {
         int res = 0;
+
         for (int i = 1; i <= n; i++) {
             int sq = i * i;
+            mp.clear();
             string ss = to_string(sq);
             if (check(ss, i, 0, 0)) {
                 res += sq;
