87663a6092b24ccf40ffce0522282ee6d179d81d
Amber Singh <ambersinghrok@gmail.com>
2025-02-15 20:10:31 -0500

    Time: 0 ms (100.00%) | Memory: 11.4 MB (84.46%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    int map_func(string s, int n){
-        unordered_map<char,int> mp;
-        int i=0,j=0,res=0;
-        while(j<n){
+    int map_func(string s, int n) {
+        unordered_map<char, int> mp;
+        int i = 0, j = 0, res = 0;
+        while (j < n) {
             mp[s[j]]++;
-            if(mp.size()==j-i+1){
-                res=max(res,j-i+1);
-                
-            }else {
-                while(mp.size()<j-i+1)
-                {mp[s[i]]--;
-                if(mp[s[i]]==0){
-                    mp.erase(s[i]);
+            if (mp.size() == j - i + 1) {
+                res = max(res, j - i + 1);
+            } else {
+                while (mp.size() < j - i + 1) {
+                    mp[s[i]]--;
+                    if (mp[s[i]] == 0) {
+                        mp.erase(s[i]);
+                    }
+                    i++;
                 }
-                i++;
-                }
-                
             }
             j++;
         }
@@ -25,15 +23,15 @@ public:
     int lengthOfLongestSubstring(string s) {
         int n = s.size();
         // return map_func(s,n);
-        vector<int> hash(256,-1);
-        int l=0,r=0,res = 0;
-        while(r<n){
-            if(hash[s[r]] != -1){
-                if(hash[s[r]] >= l){
+        vector<int> hash(256, -1);
+        int l = 0, r = 0, res = 0;
+        while (r < n) {
+            if (hash[s[r]] != -1) {
+                if (hash[s[r]] >= l) {
                     l = hash[s[r]] + 1;
                 }
             }
-            res = max(res,r-l+1);
+            res = max(res, r - l + 1);
             hash[s[r]] = r;
             r++;
         }
