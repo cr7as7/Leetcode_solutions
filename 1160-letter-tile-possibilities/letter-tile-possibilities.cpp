c765f355a9a8c1610a3349e1a503a32049483aed
Amber Singh <ambersinghrok@gmail.com>
2025-02-17 09:44:27 -0500

    Time: 4 ms (62.18%) | Memory: 7.8 MB (97.61%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
@@ -1,5 +1,17 @@
 class Solution {
 public:
+    int ans;
+    void solve2(vector<int>& vec) {
+        ans++;
+        for (int j = 0; j < 26; j++) {
+            if (vec[j]==0)
+                continue;
+            vec[j]--;
+            solve2(vec);
+            vec[j]++;
+        }
+        return;
+    }
     void solve(string& tiles, unordered_set<string>& st, string& ans,
                vector<bool>& vis) {
         if (!ans.empty()) {
@@ -18,10 +30,17 @@ public:
         return;
     }
     int numTilePossibilities(string tiles) {
-        unordered_set<string> st;
-        string ans = "";
-        vector<bool> vis(tiles.size(), false);
-        solve(tiles, st, ans, vis);
-        return st.size();
+        // unordered_set<string> st;
+        // string ans = "";
+        // vector<bool> vis(tiles.size(), false);
+        vector<int> vec(26,0);
+        for(char ch: tiles){
+            vec[ch-'A']++;
+        }
+        ans =0;
+        // solve(tiles, st, ans, vis);
+        solve2(vec);
+        // return st.size();
+        return ans-1;
     }
 };
