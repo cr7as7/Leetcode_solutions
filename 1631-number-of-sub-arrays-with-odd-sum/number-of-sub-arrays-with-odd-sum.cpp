6d7781493007a5c276df225f0bf8e0d2f7aa4e9b
Amber Singh <ambersinghrok@gmail.com>
2025-02-25 10:11:30 -0500

    Time: 3 ms (73.02%) | Memory: 112 MB (80.00%) - LeetSync

--- 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     int numOfSubarrays(vector<int>& arr) {
         int n = arr.size();
-        vector<int> pref(n,0);
-        pref[0]=arr[0];
-        for(int i=1;i<n;i++) pref[i] = pref[i-1] + arr[i];
+        // vector<int> pref(n,0);
+        // pref[0]=arr[0];
+        // for(int i=1;i<n;i++) pref[i] = pref[i-1] + arr[i];
 
-        int even = 1,odd=0,count=0,m=1e9+7;
+        int even = 1,odd=0,count=0,m=1e9+7,sum=0;
         for(int i=0;i<n;i++){
-            if(pref[i]%2==0){
+            sum+=arr[i];
+            if(sum%2==0){
                 count = (count+odd) % m;
                 even++;
             }else{
