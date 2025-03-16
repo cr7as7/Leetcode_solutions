5f154580ef46741113a80bc65d8cc6ac6cf0392f
Amber Singh <ambersinghrok@gmail.com>
2025-03-16 12:04:55 -0400

    Time: 47 ms (64.22%) | Memory: 59.4 MB (45.76%) - LeetSync

---
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    typedef long long ll;
+    bool isPossible(vector<int>& ranks, ll mid, int cars) {
+        int n = ranks.size();
+        ll cars_fixed = 0;
+        for (int i = 0; i < n; i++) {
+            cars_fixed += sqrt(mid / ranks[i]);
+        }
+        return cars_fixed >= cars;
+    }
+    long long repairCars(vector<int>& ranks, int cars) {
+        int n = ranks.size();
+        int maxi = *max_element(begin(ranks), end(ranks));
+        ll l = 1;
+        ll r = 1LL * maxi * cars * cars;
+        ll ans = r;
+        while (l <= r) {
+            ll mid = l + (r - l) / 2;
+            if (isPossible(ranks, mid, cars)) {
+                ans = mid;
+                r = mid - 1;
+            } else
+                l = mid + 1;
+        }
+        return ans;
+    }
+};
