d3cf8b3c5ff9bd1244f0e94ed210e1b4160ef9c3
Amber Singh <ambersinghrok@gmail.com>
2025-04-01 09:30:41 -0400

    Time: 9 ms (39.19%) | Memory: 127.7 MB (16.19%) - LeetSync

---
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    typedef long long ll;
+    
+    ll solve(int i,vector<vector<int>>& q,ll & n,vector<ll> & dp){
+        if(i>=n) return 0;
+        if(dp[i]!=-1) return dp[i];
+        ll taken = q[i][0] + solve(i+q[i][1]+1,q,n,dp);
+        ll not_taken = solve(i+1,q,n,dp);
+        return dp[i]=max(taken,not_taken);
+    }
+    long long mostPoints(vector<vector<int>>& questions) {
+        ll n = questions.size();
+        vector<ll> dp(n+1,-1);
+        return solve(0,questions,n,dp);
+    }
+};
