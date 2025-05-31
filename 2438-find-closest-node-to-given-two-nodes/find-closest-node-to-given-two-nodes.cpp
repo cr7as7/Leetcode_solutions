9227ce0f9e32a488a81de5d41b605599986e7838
Amber Singh <ambersinghrok@gmail.com>
2025-05-30 19:25:51 -0500

    Time: 15 ms (68.81%) | Memory: 102.2 MB (69.29%) - LeetSync

---
+++ 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void dfs(vector<int>& edges,int node,vector<bool> & visited,vector<int> & dist){
+        visited[node]=true;
+        int v = edges[node];
+        if(v != -1 && !visited[v]){
+            dist[v] = 1+dist[node];
+            dfs(edges,v,visited,dist);
+        }
+    }
+    int closestMeetingNode(vector<int>& edges, int node1, int node2) {
+        int n = edges.size();
+        vector<int> dist1(n,INT_MAX);
+        vector<int> dist2(n,INT_MAX);
+        dist1[node1]=0;
+        dist2[node2]=0;
+        vector<bool> visited1(n,false);
+        vector<bool> visited2(n,false);
+        dfs(edges,node1,visited1,dist1);
+        dfs(edges,node2,visited2,dist2);
+        int minNode = -1;
+        int minDistTillNow = INT_MAX;
+        for(int i=0;i<n;i++){
+            int maxi = max(dist1[i],dist2[i]);
+            if(minDistTillNow > maxi){
+                minDistTillNow = maxi;
+                minNode = i;
+            }
+        }
+        return minNode;
+        
+        
+    }
+};
