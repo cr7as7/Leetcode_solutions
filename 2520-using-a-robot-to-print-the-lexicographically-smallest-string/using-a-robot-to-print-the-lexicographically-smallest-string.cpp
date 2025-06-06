0c04e9ba181eaaf62cdb674708ab98fb1056d7e4
Amber Singh <ambersinghrok@gmail.com>
2025-06-06 11:13:32 -0400

    Time: 55 ms (57.92%) | Memory: 37.3 MB (21.29%) - LeetSync

--- 2520-using-a-robot-to-print-the-lexicographically-smallest-string/using-a-robot-to-print-the-lexicographically-smallest-string.cpp
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/using-a-robot-to-print-the-lexicographically-smallest-string.cpp
@@ -3,24 +3,24 @@ public:
     string robotWithString(string s) {
         int n = s.length();
         vector<char> minCharToRight(n);
-        minCharToRight[n-1]=s[n-1];
-        for(int i=n-2;i>=0;i--){
-            minCharToRight[i]=min(s[i],minCharToRight[i+1]);
+        minCharToRight[n - 1] = s[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            minCharToRight[i] = min(s[i], minCharToRight[i + 1]);
         }
-        int i=0;
+        int i = 0;
         string t = "";
         string paper = "";
-        while(i<n){
+        while (i < n) {
             t.push_back(s[i]);
-            char mini = (i+1<n) ? minCharToRight[i+1]: s[i];
+            char mini = (i + 1 < n) ? minCharToRight[i + 1] : s[i];
 
-            while(!t.empty() && t.back() <= mini){
+            while (!t.empty() && t.back() <= mini) {
                 paper.push_back(t.back());
                 t.pop_back();
             }
             i++;
         }
-        while(!t.empty()){
+        while (!t.empty()) {
             paper.push_back(t.back());
             t.pop_back();
         }
