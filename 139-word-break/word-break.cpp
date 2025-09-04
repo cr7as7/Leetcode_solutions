46033d5a5ee2b6866d23081adc8eff233073407b
Amber Singh <ambersinghrok@gmail.com>
2025-09-04 19:58:18 -0400

    Time: 14 ms (37.06%) | Memory: 17 MB (33.56%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    unordered_map<string, bool> m;
-    bool solve(string s, unordered_set<string>& se) {
-        if (se.find(s) != se.end())
-            return true;
-        if (m.find(s) != m.end())
-            return m[s];
-        for (int i = 0; i < s.length(); i++) {
-            string s1 = s.substr(0, i);
-            if (se.find(s1) != se.end() && solve(s.substr(i), se)) {
+    unordered_map<int, bool> m;  // memo: idx -> canBreak
 
-                return m[s] = true;
+    bool solve(string &s, int idx, unordered_set<string>& se) {
+        int n = s.length();
+        if (idx == n) return true;                 // reached end -> valid
+        if (m.find(idx) != m.end()) return m[idx];
+
+        // try every prefix starting at idx
+        for (int i = idx; i < n; i++) {
+            // s[idx..i]
+            string pref = s.substr(idx, i - idx + 1);
+            if (se.find(pref) != se.end() && solve(s, i + 1, se)) {
+                return m[idx] = true;
             }
         }
-
-        return m[s] = false;
+        return m[idx] = false;
     }
+
     bool wordBreak(string s, vector<string>& wordDict) {
         unordered_set<string> se;
-        for (auto a : wordDict)
-            se.insert(a);
-        return solve(s, se);
+        for (auto &a : wordDict) se.insert(a);
+        return solve(s, 0, se);
     }
 };
