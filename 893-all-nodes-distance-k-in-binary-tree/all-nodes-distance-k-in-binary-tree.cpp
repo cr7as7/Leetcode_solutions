264acc73ffeafd427b0ce218c400fae4761f1da6
Amber Singh <ambersinghrok@gmail.com>
2025-10-05 15:13:06 -0400

    Time: 6 ms (37.84%) | Memory: 15.5 MB (32.79%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -7,68 +7,57 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution {
 public:
-    void markParent(TreeNode* root, unordered_map<TreeNode*, TreeNode*>& mp) {
-        queue<TreeNode*> q;
-        q.push(root);
-        while (!q.empty()) {
-            TreeNode* curr = q.front();
-            q.pop();
-            if (curr->left) {
-                q.push(curr->left);
-                mp[curr->left] = curr;
-            }
-            if (curr->right) {
-                q.push(curr->right);
-                mp[curr->right] = curr;
-            }
-        }
+    void inorderMarkParent(TreeNode* root,
+                           unordered_map<TreeNode*, TreeNode*>& parent) {
+        if (!root)
+            return;
+        if (root->left)
+            parent[root->left] = root;
+        inorderMarkParent(root->left, parent);
+        if (root->right)
+            parent[root->right] = root;
+        inorderMarkParent(root->right, parent);
     }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        unordered_map<TreeNode*, TreeNode*> mp;
-        markParent(root, mp);
-        unordered_map<TreeNode*, bool> vis;
+        unordered_map<TreeNode*, TreeNode*> parent;
+        inorderMarkParent(root, parent);
         queue<TreeNode*> q;
+        unordered_set<TreeNode*> vis;
+        vis.insert(target);
         q.push(target);
-        vis[target] = true;
-        int curr_lvl = 0;
-        while (!q.empty()) {
-            int s = q.size();
-            if (curr_lvl++ == k)
-                break;
-            while (s--) {
-                TreeNode* curr = q.front();
+        int l = 0;
+        while(!q.empty()){
+            int n = q.size();
+            if(k==0) break;
+            while(n--){
+                l++;
+                auto t = q.front();
                 q.pop();
-                if (curr->left && !vis[curr->left]) {
-                    q.push(curr->left);
-                    vis[curr->left] = true;
+                if(t->left != NULL && !vis.count(t->left)){
+                    q.push(t->left);
+                    vis.insert(t->left);
                 }
-                if (curr->right && !vis[curr->right]) {
-                    q.push(curr->right);
-                    vis[curr->right] = true;
+                if(t->right != NULL && !vis.count(t->right)){
+                    q.push(t->right);
+                    vis.insert(t->right);
                 }
-                if (mp[curr] && !vis[mp[curr]]) {
-                    q.push(mp[curr]);
-                    vis[mp[curr]] = true;
+                if(parent[t] != NULL && !vis.count(parent[t])){
+                    q.push(parent[t]);
+                    vis.insert(parent[t]);
                 }
+                
             }
+            k--;
         }
-        vector<int> result;
-        while (!q.empty()) {
-            TreeNode* current = q.front();
+        vector<int> res;
+        while(!q.empty()){
+            auto t= q.front();
             q.pop();
-            result.push_back(current->val);
+            if(t) res.push_back(t->val);
         }
-        return result;
+
+        return res;
     }
 };
